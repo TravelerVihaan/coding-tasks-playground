160cb00f174b05a746259291f24a7678eb6e5e7b
Vihaan <vihaan.1312@outlook.com>
2026-08-26 00:18:35 +0200

    pom refactor + disable tests for unfinished coding tasks

--- src/test/java/com/github/vihaan/codewars/kyu3/BattleFieldTest.java
+++ src/test/java/com/github/vihaan/codewars/kyu3/BattleFieldTest.java
@@ -1,11 +1,11 @@
 package com.github.vihaan.codewars.kyu3;
 
-import org.junit.jupiter.api.Disabled;
+import com.github.vihaan.util.UnfinishedTask;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled("Unfinished coding task")
+@UnfinishedTask
 public class BattleFieldTest {
 
   private static int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
