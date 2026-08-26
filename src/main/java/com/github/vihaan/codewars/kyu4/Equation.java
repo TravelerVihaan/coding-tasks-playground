35700d15512684f661f1d364523f1cab0236af54
Vihaan <vihaan.1312@outlook.com>
2026-08-26 10:24:17 +0200

    Differentiate a polynomial task (Equation.java)

--- src/main/java/com/github/vihaan/codewars/kyu4/Equation.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/Equation.java
@@ -2,19 +2,19 @@ package com.github.vihaan.codewars.kyu4;
 
 import java.math.BigInteger;
 
-/*
-Create a function that differentiates a polynomial for a given value of x.
-
-Your function will receive 2 arguments: a polynomial as a string, and a point to evaluate the equation as an integer.
-
-Assumptions:
-There will be a coefficient near each x, unless the coefficient equals 1 or -1.
-There will be an exponent near each x, unless the exponent equals 0 or 1.
-All exponents will be greater or equal to zero
-Examples:
-Equation.differenatiate("12x+2", 3)      ==>   12
-Equation.differenatiate("x^2+3x+2", 3)   ==>   9
- */
+/// Create a function that differentiates a polynomial for a given value of x.
+///
+/// Your function will receive 2 arguments: a polynomial as a string, and a point to evaluate the equation as an integer.
+/// Assumptions:
+///
+/// There will be a coefficient near each `x`, unless the coefficient equals `1` or `-1`.
+///     There will be an exponent near each `x`, unless the exponent equals `0` or `1`.
+///     All exponents will be greater or equal to zero
+///
+/// Examples:
+///```
+/// Equation.differenatiate("12x+2", 3)      ==>   12
+/// Equation.differenatiate("x^2+3x+2", 3)   ==>   9```
 public class Equation {
     
     public static BigInteger differentiate(String equation, long x) {
--- src/test/java/com/github/vihaan/codewars/kyu4/EquationTest.java
+++ src/test/java/com/github/vihaan/codewars/kyu4/EquationTest.java
@@ -1,14 +1,13 @@
 package com.github.vihaan.codewars.kyu4;
 
-import com.github.vihaan.util.UnfinishedTask;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@UnfinishedTask
 public class EquationTest {
+
     @Test
     public void sampleTests() {
         assertEquals(new BigInteger("12"),  Equation.differentiate("12x+2", 3));
